6152f74275f1eedc67ac8bad82f9d4a665dff6df
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2023-01-08 00:19:23 +0800

    fix: 修复路由报错

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from 'react-dom'
-import AppRouter from './router';
+import React, { Suspense } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from '@/views/App';
 
-import './styles/index.less'
+import './styles/index.less';
 
-ReactDOM.render(
-  <AppRouter />,
-  document.getElementById('root')
-)
+const container: any = document.getElementById('root');
+const root = createRoot(container);
+
+if (root) {
+  root.render(
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,52 +1,24 @@
-import * as React from "react";
-import Layout from "../layout";
-import { RouterConfig } from "../types/router";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import * as React from 'react';
+import type { RouteProps } from 'react-router-dom';
+import Layout from '../layout';
+import StartPage from '@/views/start';
+import WorkPage from '@/views/work';
 
-const router = [
+const routers = [
   {
-    path: '/app',
-    component: () => import('../views/App'),
-    meta: {
-      title: '首页'
-    }
-  }
-] as unknown as RouterConfig
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/start',
+        element: <StartPage />,
+      },
+      {
+        path: '/work',
+        element: <WorkPage />,
+      },
+    ],
+  },
+] as RouteProps;
 
-const renderRoutes = (routes: any) => {
-  // if (!Array.isArray(routes)) {
-  //   return null
-  // }
-
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {
-          routes.map((route: any, index: number) => {
-            const { path, component, meta } = route
-            console.log(route);
-            return (
-              <Route
-                key={index}
-                path={path}
-                element={React.createElement(component, { meta })}
-              />
-            )
-          })
-        }
-      </Route>
-    </Routes>
-  )
-}
-
-const AppRouter = () => {
-  return (
-    <React.StrictMode>
-      <BrowserRouter>
-        {renderRoutes(router)}
-      </BrowserRouter>
-    </React.StrictMode>
-  )
-}
-
-export default AppRouter
+export default routers;
